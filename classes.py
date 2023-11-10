4a81339f14cd02e12cdcfe6a390032e84bf28b48
Aleksandr Buganov <rivares89@gmail.com>
2023-11-11 02:40:48 +0300

    fix formatted

--- classes.py
+++ classes.py
@@ -1,5 +1,7 @@
 import json
 from abc import ABC, abstractmethod
+from datetime import datetime
+
 from exception import ParsingError
 import os
 # from dotenv import load_dotenv
@@ -19,26 +21,27 @@ class HeadHunter(Engine):
 
     def __init__(self, keyword):
         self.params = {
-            # "count": 1,
-            # "per_page": None,
-            # "name": keyword,
-            # "archive": False
+            "text": keyword,
+            "page": None,
+            "per_page": 5
         }
         self.vacancies = []
 
     def get_request(self):
+        '''Делает запрос и возвращает в формате json'''
         response = requests.get(f'{self.url}/vacancies', params=self.params)
         if response.status_code != 200:
             raise ParsingError(f"Ошибка получения вакансий! Статус: {response.status_code}")
-        print(response.json())
+        # print(response.json())
         return response.json()
 
 
-    def get_vacancies(self, pages_count=1):
+    def get_vacancies(self, pages_count=10):
+        '''Проходим циклом по словарю берем из словаря только нужные нам данные и записываем их в переменную "vacancies" '''
         self.vacancies = [] # очищаем список
         for page in range(pages_count):
             page_vacancies = []
-            self.params["per_page"] = page
+            # self.params["per_page"] = page
             print(f"({self.__class__.__name__}) Парсинг страницы {page} -", end=" ")
             try:
                 page_vacancies = self.get_request()
@@ -51,6 +54,7 @@ class HeadHunter(Engine):
                 # print(json.dumps(page_vacancies['items'], indent=2, ensure_ascii=False))
                 self.vacancies.extend(page_vacancies['items'])
                 # print(json.dumps(self.vacancies, indent=2, ensure_ascii=False))
+                # print(f"Загружено вакансий {len(self.vacancies)}")
                 print(f"Загружено вакансий {len(page_vacancies['items'])}")
                 # print(len(self.vacancies))
                 # print(self.vacancies)
@@ -62,15 +66,20 @@ class HeadHunter(Engine):
     def get_formatted_vacancies(self):
         formatted_vacancies = []
         for vacancy in self.vacancies:
+            published_at = datetime.strptime(vacancy['published_at'], "%Y-%m-%dT%H:%M:%S%z")
             # print(vacancy)
             # print(json.dumps(vacancy, indent=2, ensure_ascii=False))
             # print(f"New")
             formatted_vacancy = {
-                "ist": "hh.ru",
-                "employer": vacancy['department'],
+                # "ist": "hh.ru",
+                "employer": vacancy['department']['name'] if vacancy.get('department') else None,
                 "title": vacancy['name'],
-                "payment_from": vacancy['salary']['from'],
-                "payment_to": vacancy['salary']['to'],
+                "payment_from": vacancy['salary']['from'] if vacancy.get('salary') else None,
+                "payment_to": vacancy['salary']['to'] if vacancy.get('salary') else None,
+                'responsibility': vacancy['snippet']['responsibility'],
+                'link': vacancy['apply_alternate_url'],
+                'date': published_at.strftime("%d.%m.%Y"),
+
             }
             formatted_vacancies.append(formatted_vacancy)
         return formatted_vacancies
@@ -80,7 +89,7 @@ class SuperJob(Engine):
 
     def __init__(self, keyword):
         self.params = {
-            "count": 100,
+            "count": 2,
             "page": None,
             "keyword": keyword,
             "archive": False
@@ -88,7 +97,6 @@ class SuperJob(Engine):
 
         self.headers = {
             "X-Api-App-Id": "v3.r.137936779.c561df967b4d14c5d30f8b6e3132e9b7bc52cb7f.e8b0c26ac031c925476eba981ecfa9c3dcd8a66f"
-
         }
         self.vacancies = []
 
@@ -104,10 +112,11 @@ class SuperJob(Engine):
         self.vacancies = [] # очищаем список
         for page in range(pages_count):
             page_vacancies = []
-            self.params["page"] = page
+            # self.params["page"] = page
             print(f"({self.__class__.__name__}) Парсинг страницы {page} -", end=" ")
             try:
                 page_vacancies = self.get_request()
+                print(json.dumps(page_vacancies, indent=2, ensure_ascii=False))
             except ParsingError as error:
                 print(error)
             else:
@@ -121,11 +130,15 @@ class SuperJob(Engine):
     def get_formatted_vacancies(self):
         formatted_vacancies = []
         for vacancy in self.vacancies:
+            published_at = datetime.fromtimestamp(vacancy.get('date_published', ''))
             formatted_vacancy = {
                 "employer": vacancy["firm_name"],
                 "title": vacancy["profession"],
                 "payment_from": vacancy["payment_from"],
                 "payment_to": vacancy["payment_to"],
+                'responsibility': vacancy.get('candidat').replace('\n', '').replace('•', '') if vacancy.get('candidat') else None,
+                'link': vacancy['client']['link'],
+                'date': published_at.strftime("%d.%m.%Y"),
             }
             formatted_vacancies.append(formatted_vacancy)
         return formatted_vacancies
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     connector.insert(vacancies_json)
 
     hh = HeadHunter(keyword)
-    hh.get_vacancies(pages_count=2)
+    hh.get_vacancies(pages_count=1)
     vacancies_json.extend(hh.get_formatted_vacancies())
 
     connector = Connector(keyword=keyword)
